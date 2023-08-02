40a4c0e2d68d174c546248bad9fd92e376533204
Gabriel <gabrielmedinatorres@hotmail.com>
2023-08-02 19:44:52 +0100

    dhfg

--- pruebas Boostrab react/pruebasboostrap/src/App.js
+++ pruebas Boostrab react/pruebasboostrap/src/App.js
@@ -3,12 +3,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Carousel from "react-bootstrap/Carousel";
 import Carrosuel from "./components/Carrosuel";
 import OffcanvasExample from "./components/OffcanvasExample";
+import Acordeon from "./components/Acordeon";
 
 function App() {
   return (
     <div className="App">
       <OffcanvasExample />
-      <Carrosuel />
+
+      <Acordeon />
     </div>
   );
 }
